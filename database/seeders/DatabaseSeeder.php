26a040ccff3c1b3db7019d2ac123ef24bdff10c6
Tri Putra Satriawan <tripsatriawan30@gmail.com>
2025-08-07 23:16:12 +0700

    Update Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -10,7 +10,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             UsersTableSeeder::class,
-            ProductsTableSeeder::class,
+            // ProductsTableSeeder::class,
         ]);
     }
 }
